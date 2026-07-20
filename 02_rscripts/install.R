29f09c074d9df571bee172c23d428304cae5a7fd
Ghd <gracia.brueckmann@unibe.ch>
2026-07-20 18:34:39 +0200

    back to basics

--- 02_rscripts/install.R
+++ 02_rscripts/install.R
@@ -4,9 +4,7 @@
 packs <- c('dataverse', 'causaldata', 'data.table', 'estimatr', 'evaluate', 'fixest', 'ggplot2',
            'haven', 
             'magrittr', 'modelsummary', 'openssl', 'ragg',
-           'softbib', 'stringr', 'this.path', 'tinytable', 'xfun',
-          # 'lfe',
-           'holepunch'
+           'softbib', 'stringr', 'this.path', 'tinytable', 'xfun'
            )
 
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/2026-07-17"))
