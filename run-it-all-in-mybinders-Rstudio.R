725949e46d8352d8b10ecd0567ba471deec6baed
Ghd <gracia.brueckmann@unibe.ch>
2026-07-21 01:38:20 +0200

    added quarto to the packages to be run

--- run-it-all-in-mybinders-Rstudio.R
+++ run-it-all-in-mybinders-Rstudio.R
@@ -9,3 +9,5 @@ source("02_rscripts/01_data_cleaning.R")
 source("02_rscripts/02_descriptives.R")
 source("02_rscripts/03_modeling.R")
 source("02_rscripts/98_sessioninfo_and_cite.R")
+
+quarto::quarto_render("03_output/Comparison.qmd", quiet = TRUE)
