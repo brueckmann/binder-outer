007c000d7fd48de73a8a1a9676c1f229739894b9
Ghd <gracia.brueckmann@unibe.ch>
2026-07-20 20:31:41 +0200

    double quotes recommended

--- 02_rscripts/install.R
+++ 02_rscripts/install.R
@@ -1,22 +1,23 @@
 ### install packages
 packs <- c(
-  'dataverse',
-  'causaldata',
-  'data.table',
-  'estimatr',
-  'evaluate',
-  'fixest',
-  'ggplot2',
-  'haven',
-  'magrittr',
-  'modelsummary',
-  'openssl',
-  'ragg',
-  'softbib',
-  'stringr',
-  'this.path',
-  'tinytable',
-  'xfun'
+  "dataverse",
+  "causaldata",
+  "data.table",
+  "estimatr",
+  "evaluate",
+  "fixest",
+  "ggplot2",
+  "haven",
+  "magrittr",
+  "modelsummary",
+  "openssl",
+  "ragg",
+  "softbib",
+  "stringr",
+  "this.path",
+  "tinytable",
+  "xfun",
+  "grateful"
 )
 
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/2026-07-17"))
